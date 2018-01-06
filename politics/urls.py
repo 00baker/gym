3acb4190929a6a665bc242029a53160bd36a509c
Rajat Singh <rajatsingh.b115@gmail.com>
2018-01-06 13:03:21 +0530

    Update urls.py

--- politics/urls.py
+++ politics/urls.py
@@ -3,7 +3,6 @@ from politics import views
 
 urlpatterns = [
     url(r'^$', views.index,name='politics'),
-    url(r'^blog/(?P<slug>[\w-]+)/$', views.view_post, name='view_blog_post'),
-    url(r'^blog/category/(?P<slug>[\w-]+)/$', views.view_category, name='view_blog_category')
+    url(r'^blog/(?P<slug>[\w-]+)/$', views.view_post, name='view_politics_post'),
+    url(r'^blog/category/(?P<slug>[\w-]+)/$', views.view_category, name='view_politics_category')
 ]
-
