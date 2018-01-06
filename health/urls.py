9d87ddfd275beaa6bc69351cbebe4831e221e8ad
Rajat Singh <rajatsingh.b115@gmail.com>
2018-01-06 13:01:24 +0530

    Update urls.py

--- health/urls.py
+++ health/urls.py
@@ -3,7 +3,6 @@ from health import views
 
 urlpatterns = [
     url(r'^$', views.health,name='health'),
-    url(r'^(?P<slug>[\w-]+)/$', views.health_post, name='view_health_post'),
-    url(r'^category/(?P<slug>[\w-]+)/$', views.health_category, name='view_health_category')
+    url(r'^blog/(?P<slug>[\w-]+)/$', views.health_post, name='view_health_post'),
+    url(r'^blog/category/(?P<slug>[\w-]+)/$', views.health_category, name='view_health_category')
 ]
-
