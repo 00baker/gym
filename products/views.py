0247e97ef9893f82d9f8a30a0a2561b6a8064776
Gaurav <gaurav.bhagat1010@gmail.com>
2018-01-18 23:14:24 +0530

    updated

--- products/views.py
+++ products/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 def product(request):
     return render_to_response('product/index.html', {
         'categories': Category.objects.all(),
-        'products': Product.objects.all()[:5]
+        'products': Product.objects.all().order_by("-addded")[:5]
     })
 
 def view_product(request, slug):
@@ -16,5 +16,5 @@ def product_category(request, slug):
     category = get_object_or_404(Category, slug=slug)
     return render_to_response('product/view_category.html', {
         'category': category,
-        'products': Product.objects.filter(category=category)[:5]
+        'products': Product.objects.filter(category=category).order_by("-addded")[:5]
     })
